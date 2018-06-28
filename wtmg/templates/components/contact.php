071e2c7ad39ebc33b11bc59d37894b2d4fa023e1
maomer <maomer@kth.se>
2018-06-28 13:06:07 +0200

    added three components

---
+++ wtmg/templates/components/contact.php
@@ -0,0 +1,18 @@
+<div class="contact-inner text-over-image">
+    <h3 class="contact-input">Kontakta oss: </h3>
+    <div class="input-elements">
+        <div class="input-element">
+            <input type="text" placeholder="Namn">
+            <hr>
+        </div>
+        <div class="input-element">
+            <input type="text" placeholder="Telefonnummer">
+            <hr>
+        </div>
+        <div class="input-element">
+            <input type="text" placeholder="Epostaddress">
+            <hr>
+        </div>
+    </div>
+    <a class="button contact-input" href="">Skicka</a>
+</div>
---
+++ wtmg/templates/components/info.php
@@ -0,0 +1,7 @@
+<div class="info-inner">
+    <img class="hero-image" src="/ekonav/wp-content/themes/wtmg/assets/images/hero-placeholder.jpg" alt="" style="height:100%">
+    <div class="text-over-image">
+        <h2>Din företagsekonomi är vår passion</h2>
+        <a class="button" href="">Se vad vi kan göra för dig</a>
+    </div>
+</div>
